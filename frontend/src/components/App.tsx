53cd512dbfee5a7ffe715b2126fe3a42c6b8784a
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-06-23 17:04:44 +0200

    Add buttons

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -11,6 +11,10 @@ function App() {
             <p>track and split expenses with your friends and family.</p>
             <p>create an account for free to receive and send payments today.</p>
           </div>
+          <div className='buttons'>
+            <button type='button' className='btn btn-secondary'>Log in</button>
+            <button type='button' className='btn btn-secondary'>Sign up</button>
+          </div>
         </div>
         <img src={image} id='image'/>
       </div>
