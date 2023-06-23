b1017f84e4f39a89f0655d23adf26cd875898f80
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-06-23 15:56:08 +0200

    Add illustration

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,12 +1,18 @@
 import './App.scss'
+import image from '../assets/friends.svg'
 
 function App() {
   return (
     <div className='center'>
-      <h1 id='title'>coinmates</h1>
-      <div className='description'>
-        <p>track and split expenses with your friends and family.</p>
-        <p>create an account for free to receive and send payments today.</p>
+      <div className='flex-container'>
+        <div className='left-part'>
+          <h1 id='title'>coinmates</h1>
+          <div className='description'>
+            <p>track and split expenses with your friends and family.</p>
+            <p>create an account for free to receive and send payments today.</p>
+          </div>
+        </div>
+        <img src={image} id='image'/>
       </div>
     </div>
   )
