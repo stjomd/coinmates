359a20b354e833db07abb38aaf1a8720c4eb0232
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-06-25 18:10:53 +0200

    Remove current security implementation

--- backend/src/main/java/at/stjomd/coinmatesserver/service/user/UserServiceImpl.java
+++ backend/src/main/java/at/stjomd/coinmatesserver/service/user/UserServiceImpl.java
@@ -2,31 +2,18 @@ package at.stjomd.coinmatesserver.service.user;
 
 import at.stjomd.coinmatesserver.entity.User;
 import at.stjomd.coinmatesserver.repository.UserRepository;
-import at.stjomd.coinmatesserver.service.jwt.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-    private final JwtService jwtService;
 
     @Autowired
-    public UserServiceImpl(
-        UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager,
-        JwtService jwtService
-    ) {
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.authenticationManager = authenticationManager;
-        this.jwtService = jwtService;
     }
 
     @Override
@@ -39,15 +26,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User register(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+        return user;
     }
 
+    @Override
     public String authenticate(User user) {
-         authenticationManager.authenticate(
-             new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
-         );
-        return jwtService.constructToken(null, user);
+        return "Hello";
     }
 
 }
