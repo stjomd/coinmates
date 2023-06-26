d811ac17bde55de368fdbc61f95a494be52504c0
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-06-26 17:52:33 +0200

    Add login form

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+import './Login.scss';
+
 function Login() {
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("")
+
 	return (
-		<div>
-			Hello
+		<div className='login-box'>
+			<p className="title">coinmates</p>
+			<form onSubmit={e => e.preventDefault()}>
+				<input type="text" required
+					placeholder='Email'
+					className='form-control'
+					value={email}
+					onChange={event => setEmail(event.target.value)}
+				/>
+				<input type="password" required
+					placeholder='Password'
+					className='form-control'
+					value={password}
+					onChange={event => setPassword(event.target.value)}
+				/>
+			</form>
+			<button type='button' className='btn btn-primary login-btn'>
+				Sign in
+			</button>
+			<p>Do not have an account? Sign up</p>
 		</div>
 	);
 }
