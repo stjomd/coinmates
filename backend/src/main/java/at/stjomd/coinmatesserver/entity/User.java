ae9815850be387f3517032d56f4b35304ffe5527
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-06-26 18:29:42 +0200

    Implement basic auth logic

--- backend/src/main/java/at/stjomd/coinmatesserver/entity/User.java
+++ backend/src/main/java/at/stjomd/coinmatesserver/entity/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -15,7 +16,7 @@ import lombok.Data;
 public class User {
 
     public enum Role {
-        REGULAR, ADMIN;
+        REGULAR, ADMIN
     }
 
     public enum Status {
@@ -23,7 +24,7 @@ public class User {
     }
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(length = 100, nullable = false)
--- backend/src/main/java/at/stjomd/coinmatesserver/service/user/UserServiceImpl.java
+++ backend/src/main/java/at/stjomd/coinmatesserver/service/user/UserServiceImpl.java
@@ -3,16 +3,19 @@ package at.stjomd.coinmatesserver.service.user;
 import at.stjomd.coinmatesserver.entity.User;
 import at.stjomd.coinmatesserver.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -22,12 +25,21 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User register(User user) {
-        return user;
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        // Check that no user with the same email exists
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            return null;
+        }
+        return userRepository.save(user);
     }
 
     @Override
     public String authenticate(User user) {
-        return "Hello";
+        User foundUser = userRepository.findByEmail(user.getEmail()).orElse(null);
+        if (foundUser != null && passwordEncoder.matches(user.getPassword(), foundUser.getPassword())) {
+            return foundUser.getId().toString();
+        }
+        return "throw exception here";
     }
 
 }
