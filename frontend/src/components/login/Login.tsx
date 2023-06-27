d0794fa824199d2b97e71fdb5c21bae2cc851e7d
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-06-27 15:57:21 +0200

    Show validation errors

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -8,16 +8,25 @@ function Login() {
 
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("")
-	const [errorMessage, setErrorMessage] = useState<string>();
 
 	const login = new LoginDetails(email, password);
-
 	useEffect(() => {
 		login.email = email;
 		login.password = password;
 	})
 
+	const [errorMessage, setErrorMessage] = useState<string>();
+
+	const [emailValidation, setEmailValidation] = useState<string>();
+	const [passwordValidation, setPasswordValidation] = useState<string>();
+
 	const authenticate = () => {
+		setErrorMessage(undefined)
+		setEmailValidation(undefined)
+		setPasswordValidation(undefined)
+		if (!validate()) {
+			return
+		}
 		UserService.authenticate(login)
 			.then(id => console.log('Hello user ' + id))
 			.catch(error => {
@@ -29,6 +38,74 @@ function Login() {
 			})
 	}
 
+	const validate = () => {
+		let valid = true
+		if (email.length < 1) {
+			setEmailValidation('Please enter an email')
+			valid = false
+		} else if (email.split('@').length !== 2) {
+			setEmailValidation('Please enter a correct email')
+			valid = false
+		} else {
+			const regex = new RegExp(
+				'^[a-zA-Z0-9.+_-]+@([a-zA-Z0-9+_-]+\.)+[a-zA-Z0-9]+$'
+			);
+			if (!regex.test(email)) {
+				setEmailValidation('Please enter a correct email')
+				valid = false
+			}
+		}
+		if (password.length < 1) {
+			setPasswordValidation('Please enter a password')
+			valid = false
+		}
+		return valid
+	}
+
+	const emailField = () => {
+		let classes = 'form-control';
+		if (emailValidation != null) {
+			classes += ' is-invalid'
+		}
+		return (
+			<>
+				<input type="text" required
+					placeholder='Email'
+					className={classes}
+					value={email}
+					onChange={event => setEmail(event.target.value)}
+				/>
+				{ emailValidation != null &&
+					<div className="invalid-feedback">
+						{ emailValidation }
+					</div>
+				}
+			</>
+		);
+	}
+
+	const passwordField = () => {
+		let classes = 'form-control';
+		if (passwordValidation != null) {
+			classes += ' is-invalid'
+		}
+		return (
+			<>
+				<input type="password" required
+					placeholder='Password'
+					className={classes}
+					value={password}
+					onChange={event => setPassword(event.target.value)}
+				/>
+				{ passwordValidation != null &&
+					<div className="invalid-feedback">
+						{ passwordValidation }
+					</div>
+				}
+			</>
+		);
+	}
+
 	return (
 		<div className='login-box'>
 			<p className="title">coinmates</p>
@@ -38,18 +115,8 @@ function Login() {
 				</div>
 			}
 			<form onSubmit={e => e.preventDefault()}>
-				<input type="text" required
-					placeholder='Email'
-					className='form-control'
-					value={email}
-					onChange={event => setEmail(event.target.value)}
-				/>
-				<input type="password" required
-					placeholder='Password'
-					className='form-control'
-					value={password}
-					onChange={event => setPassword(event.target.value)}
-				/>
+				{ emailField() }
+				{ passwordField() }
 			</form>
 			<button
 				type='button'
@@ -61,6 +128,7 @@ function Login() {
 			<p>Do not have an account? Sign up</p>
 		</div>
 	);
+
 }
 
 export default Login;
