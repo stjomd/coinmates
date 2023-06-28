e7d0ebd79a0b90676b409e2ad6a953743f09c9b7
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-06-28 21:44:38 +0200

    Color password hints

--- frontend/src/components/password-helper/PasswordHelper.tsx
+++ frontend/src/components/password-helper/PasswordHelper.tsx
@@ -6,6 +6,86 @@ interface PasswordHelperProps {
 }
 
 function PasswordHelper({ password }: PasswordHelperProps) {
+
+	// Validators
+	const isLong = () => {
+		if (password == null) {
+			return false
+		}
+		return password.length >= 10
+	}
+	const containsLowercase = () => {
+		if (password == null) {
+			return false
+		}
+		return password !== password.toUpperCase()
+	}
+	const containsUppercase = () => {
+		if (password == null) {
+			return false
+		}
+		return password !== password.toLowerCase()
+	}
+	const containsNumber = () => {
+		if (password == null) {
+			return false
+		}
+		return /[0-9]/.test(password)
+	}
+	const containsSpecialChar = () => {
+		if (password == null) {
+			return false
+		}
+		return /[!#$&*_-]/.test(password)
+	}
+
+	// Singular hints/password requirements
+	const lengthHint = () => {
+		const pClass = (isLong()) ? 'password-ok' : 'password-bad'
+		return (
+			<p className={pClass}>
+				<i className="bi bi-x"/>
+				has at least 10 characters
+			</p>
+		)
+	}
+	const lowercaseHint = () => {
+		const pClass = (containsLowercase()) ? 'password-ok' : 'password-bad'
+		return (
+			<p className={pClass}>
+				<i className="bi bi-x"/>
+				contains a lowercase letter
+			</p>
+		)
+	}
+	const uppercaseHint = () => {
+		const pClass = (containsUppercase()) ? 'password-ok' : 'password-bad'
+		return (
+			<p className={pClass}>
+				<i className="bi bi-x"/>
+				contains an uppercase letter
+			</p>
+		)
+	}
+	const numberHint = () => {
+		const pClass = (containsNumber()) ? 'password-ok' : 'password-bad'
+		return (
+			<p className={pClass}>
+				<i className="bi bi-x"/>
+				contains a number
+			</p>
+		)
+	}
+	const specialCharsHint = () => {
+		const pClass = (containsSpecialChar()) ? 'password-ok' : 'password-bad'
+		return (
+			<p className={pClass}>
+				<i className="bi bi-x"/>
+				contains a special character: !#$&*_-
+			</p>
+		)
+	}
+
 	return (
 		<div className="password-helper">
 			<div className="password-bar">
@@ -18,29 +98,15 @@ function PasswordHelper({ password }: PasswordHelperProps) {
 				</div>
 			</div>
 			<div className="password-requirements">
-				<p>
-					<i className="bi bi-x"/>
-					has at least 10 characters
-				</p>
-				<p>
-					<i className="bi bi-x"/>
-					contains a lowercase letter
-				</p>
-				<p>
-					<i className="bi bi-x"/>
-					contains an uppercase letter
-				</p>
-				<p>
-					<i className="bi bi-x"/>
-					contains a number
-				</p>
-				<p>
-					<i className="bi bi-x"/>
-					contains a special character: !#$&*_-
-				</p>
+				{ lengthHint() }
+				{ lowercaseHint() }
+				{ uppercaseHint() }
+				{ numberHint() }
+				{ specialCharsHint() }
 			</div>
 		</div>
 	);
+
 }
 
 export default PasswordHelper;
