2629bac92f7da48b710ea552d12ce28cabd367ec
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-07-01 01:40:42 +0200

    Redirect from login and sign up if already authd

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -1,14 +1,37 @@
 import './App.scss'
 import '../../styles/global.scss'
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom'
 
 import Landing from '../landing/Landing'
 import Login from '../login/Login'
 import Register from '../register/Register'
 import Header from '../header/Header'
+import {UserService} from '../../services/UserService'
+import {ReactNode} from 'react'
 
 function App() {
+	/**
+	 * Determines if the component is shown to authenticated users. Use for login,
+	 * registration, etc.
+	 * @param url the URL to redirect the user to.
+	 * @param element the element to render.
+	 * @returns if the user is logged in, redirects to the specified URL.
+	 * 					Otherwise renders the element.
+	 */
+	const onlyIfNotLoggedIn = (url: string, element: ReactNode) => {
+		if (UserService.getAuth() != null) {
+			return <Navigate replace to={url} />
+		} else {
+			return element
+		}
+	}
+
 	/**
 	 * Constructs a header element. Determines if it is shown depending on the
 	 * route.
@@ -38,32 +61,35 @@ function App() {
 				{/* home page */}
 				<Route
 					path='/'
-					element={
+					element={onlyIfNotLoggedIn(
+						'/home',
 						<div className='centered gradient'>
 							<Landing />
 						</div>
-					}
+					)}
 				/>
 				{/* login form */}
 				<Route
 					path='/login'
-					element={
+					element={onlyIfNotLoggedIn(
+						'/home',
 						<div className='centered'>
 							<Login />
 						</div>
-					}
+					)}
 				/>
 				{/* register form */}
 				<Route
 					path='/register'
-					element={
+					element={onlyIfNotLoggedIn(
+						'/home',
 						<div className='centered'>
 							<Register />
 						</div>
-					}
+					)}
 				/>
 				{/* all other routes: 404 */}
-				<Route path='*' element={<p>404</p>} />
+				<Route path='*' element={<p>Welcome!</p>} />
 			</Routes>
 		)
 	}
