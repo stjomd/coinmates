1113efc4a85261a9b47854f85f5cbdf38d1de4e6
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-07-01 18:26:27 +0200

    Add friends to user entity

--- backend/src/main/java/at/stjomd/coinmatesserver/entity/User.java
+++ backend/src/main/java/at/stjomd/coinmatesserver/entity/User.java
@@ -1,12 +1,15 @@
 package at.stjomd.coinmatesserver.entity;
 
+import java.util.Set;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -46,5 +49,8 @@ public class User {
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private Status status;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	private Set<User> friends;
 
 }
