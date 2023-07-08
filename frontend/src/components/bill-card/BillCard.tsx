ad97314a8501fe9618e3933ab22404d20b431b51
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-07-08 12:55:52 +0200

    Style bill card

--- frontend/src/components/bill-card/BillCard.tsx
+++ frontend/src/components/bill-card/BillCard.tsx
@@ -3,7 +3,24 @@ import './BillCard.scss'
 function BillCard() {
 	return (
 		<ul className='list-group'>
-			<li className='list-group-item'>Bill</li>
+			<li className='list-group-item'>
+				<div className='bc-container'>
+					<div className='bc-date'>
+						<span>8</span>
+						<span>Jul</span>
+					</div>
+					<div className='bc-details'>
+						<span className='bc-title'>
+							Bill Title{' '}
+							<span className='badge rounded-pill text-bg-primary'>Open</span>
+						</span>
+						<span className='bc-persons'>Person 1, Person 2, ...</span>
+					</div>
+					<div className='bc-price'>
+						<span className='logo'>58,00 &euro;</span>
+					</div>
+				</div>
+			</li>
 		</ul>
 	)
 }
