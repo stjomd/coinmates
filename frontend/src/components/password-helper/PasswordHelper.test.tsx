57ae6fbf6b4cb70063da57bed618b285599aa663
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2023-07-30 12:46:08 +0200

    Add password helper tests

--- frontend/src/components/password-helper/PasswordHelper.test.tsx
+++ frontend/src/components/password-helper/PasswordHelper.test.tsx
@@ -1,8 +1,29 @@
 import {render} from '@testing-library/react'
 import PasswordHelper from './PasswordHelper'
 
-test('empty password should be rejected', () => {
-	let pass = false
-	render(<PasswordHelper password='' fulfils={result => (pass = result)} />)
-	expect(pass).toBe(false)
+describe("'fulfils' callback should accept a correct pass value", () => {
+	test('false when password is empty', () => {
+		let pass = false
+		render(<PasswordHelper password='' fulfils={result => (pass = result)} />)
+		expect(pass).toBe(false)
+	})
+
+	test('false when password does not meet requirements', () => {
+		let pass = false
+		render(
+			<PasswordHelper password='hey1' fulfils={result => (pass = result)} />
+		)
+		expect(pass).toBe(false)
+	})
+
+	test('true when password meets requirements', () => {
+		let pass = false
+		render(
+			<PasswordHelper
+				password='testHey123pass!'
+				fulfils={result => (pass = result)}
+			/>
+		)
+		expect(pass).toBe(true)
+	})
 })
